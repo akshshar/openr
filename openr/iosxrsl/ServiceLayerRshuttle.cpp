fed9ce87068389da92edf2979a50c88c8d599628
Akshat Sharma <akshat31187@gmail.com>
2020-05-19 17:40:17 -0700

    Update ServiceLayerRshuttle.cpp

--- openr/iosxrsl/ServiceLayerRshuttle.cpp
+++ openr/iosxrsl/ServiceLayerRshuttle.cpp
@@ -28,6 +28,8 @@ namespace openr {
 
 std::string grpc_connectivity_state_name(grpc_connectivity_state state) {
   switch (state) {
+    case GRPC_CHANNEL_INIT:
+      return "INIT";
     case GRPC_CHANNEL_IDLE:
       return "IDLE";
     case GRPC_CHANNEL_CONNECTING:
